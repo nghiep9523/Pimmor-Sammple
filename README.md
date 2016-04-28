f1c31bfce6ded73c5b0005ffc0358f9539e214f8
Trang An <trangan@Trangs-Mac-mini.local>
2016-04-28 18:05:27 +0700

    Commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Pimmor-Sammple
-# Pimmor-Sammple
--- server.js
+++ server.js
@@ -22,6 +22,12 @@ function randomJSONObj(obj, type) {
     return jSONObj;
 }
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get("/", function(req, res) {
     res.sendFile('index.html', { root: __dirname })
 });
